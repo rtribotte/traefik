2f4940c2bf2c800a8d81ddaa346d5c81379916ce
Manuel Zapf <manuel@containo.us>
2020-12-30 14:34:55 +0100

    fix version compare

--- pkg/provider/kubernetes/ingress/client.go
+++ pkg/provider/kubernetes/ingress/client.go
@@ -546,9 +546,9 @@ func supportsIngressClass(serverVersion *version.Version) bool {
 // Ingress in networking.k8s.io/v1beta1 is deprecated starting 1.19
 // thus, we query it in K8s 1.18.
 func supportsNetworkingBetaIngress(serverVersion *version.Version) bool {
-	ingressNetworkingBetaVersion := version.Must(version.NewVersion("1.18"))
+	ingressNetworkingBetaVersion := version.Must(version.NewVersion("1.19"))
 
-	return serverVersion.LessThanOrEqual(ingressNetworkingBetaVersion)
+	return ingressNetworkingBetaVersion.GreaterThan(serverVersion)
 }
 
 //  Ingress in networking.k8s.io/v1 is supported starting 1.19.
